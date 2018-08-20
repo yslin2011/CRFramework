c3e849304117056496aedfac94ec8a6317184e85
yslin2015 <359927530@qq.com>
2018-08-20 10:25:18 +0800

    update framework

---
+++ TestFramework.framework/Headers/TestFramework.h
@@ -0,0 +1,20 @@
+//
+//  TestFramework.h
+//  TestFramework
+//
+//  Created by yslin on 2018/8/20.
+//  Copyright © 2018年 yslin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestFramework.
+FOUNDATION_EXPORT double TestFrameworkVersionNumber;
+
+//! Project version string for TestFramework.
+FOUNDATION_EXPORT const unsigned char TestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFramework/PublicHeader.h>
+
+
+#import <TestFramework/TestObj.h>
---
+++ TestFramework.framework/Headers/TestObj.h
@@ -0,0 +1,15 @@
+//
+//  TestObj.h
+//  TestFramework
+//
+//  Created by yslin on 2018/8/20.
+//  Copyright © 2018年 yslin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestObj : NSObject
+
+-(void)speak;
+
+@end
